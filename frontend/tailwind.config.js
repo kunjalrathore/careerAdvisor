fd038cc6619c5b778ec67fa77ca0c59d4a2e804b
unknown <kunjalrathore2204@gmail.com>
2025-09-22 14:26:34 +0530

    Add dotenv dependency

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+content: [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+]
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class", // 👈 enables dark mode (using a class on <html>)
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
